cefb6cf857a3cd166dd1ac3b55578d812021de92
v.boro <73988372+vboroka@users.noreply.github.com>
2022-09-09 10:52:51 +0200

    main.cpp kijavítva

--- main.cpp
+++ main.cpp
@@ -4,23 +4,29 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa";
+    for (int i = 1; i <= N_ELEMENTS; i++)
     {
+        std::cout << '\n' << i << " duplazva: ";
         b[i] = i * 2;
+        std::cout << b[i];
     }
-    for (int i = 0; i; i++)
+    std::cout << '\n';
+    for (int i = 1; i <= N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:"
-    }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
-    {
-        atlag += b[i]
+        std::cout << "\nErtek: " << i;
     }
+    
+    std::cout << "\n\nAtlag szamitasa a dupla ertekekbol: " << std::endl;
+    int atlag = 0;
+    
+    for (int i = 1; i <= N_ELEMENTS; i++){
+        atlag += b[i];
+    }
+    
     atlag /= N_ELEMENTS;
-    std::cout << "Atlag: " << atlag << std::endl;
+    std::cout << "Atlag a dupla ertekekbol: " << atlag << std::endl;
+    
     return 0;
 }
